2fd610508ef1463b365fa1525c79a1c9636924c8
Avaer Kazmer <a@modules.io>
2018-07-10 15:18:28 -0400

    Remove make clean from build script

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 source ~/emsdk/emsdk_env.sh
 export platform=emscripten
 export DEBUG=1
-make clean
+# make clean
 emmake make -j3
 cp mednafen_psx_hw_libretro_emscripten.bc ~/RetroArch/dist-scripts/
